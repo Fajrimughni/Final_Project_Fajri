a4a8a354923df19b241935c10fc8923c5c7760aa
Fajri Ilham Mughni <fajriilhammughni@gmail.com>
2025-04-18 07:48:52 +0700

    c

--- Music.py
+++ Music.py
@@ -5,7 +5,7 @@ def add_background_music(music_url):
     st.audio(music_url, format="audio/mp3")
 
 # URL file musik (pastikan URL ini dapat diakses publik)
-music_url = "https://drive.google.com/uc?id=1nHFARzdqJjtL5sbfuc2E462ke36oDx26"
+music_url = "https://drive.google.com/uc?export=download&id=1nHFARzdqJjtL5sbfuc2E462ke36oDx26"
 
 # Memanggil fungsi untuk menambahkan musik latar
 add_background_music(music_url)
--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.title("🍽️ Aplikasi Analisis & Rekomendasi Resep")
 
 # ------------------ MENAMBAHKAN MUSIK Latar ------------------ #
 # URL file musik yang sudah diubah
-music_url = "https://drive.google.com/uc?id=1nHFARzdqJjtL5sbfuc2E462ke36oDx26"
+music_url = "https://drive.google.com/uc?export=download&id=1nHFARzdqJjtL5sbfuc2E462ke36oDx26"
 
 # Menambahkan musik latar
 st.audio(music_url, format="audio/mp3", autoplay=True)
