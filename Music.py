52d4c9ede5f3aaf2fb2d887b179b2027b60b1f0c
Fajri Ilham Mughni <fajriilhammughni@gmail.com>
2025-04-18 07:42:44 +0700

    Update Music.py

--- Music.py
+++ Music.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 # Fungsi untuk menambahkan musik latar
 def add_background_music(music_file):
-    st.audio(music_file, format="audio/mp3", autoplay=True)
+    st.audio(music_file, format="audio/mp3") 
