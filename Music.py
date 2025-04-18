fd7cec75a7ea72beca89c1c532de3e0e4cdb7595
Fajri Ilham Mughni <fajriilhammughni@gmail.com>
2025-04-18 07:46:56 +0700

    Update Music.py

--- Music.py
+++ Music.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-# Fungsi untuk menambahkan musik latar
-def add_background_music(music_url):
-    st.audio(music_url, format="audio/mp3")
-
-# URL file musik (pastikan URL ini dapat diakses publik)
+# URL file musik yang sudah diubah
 music_url = "https://drive.google.com/uc?id=1nHFARzdqJjtL5sbfuc2E462ke36oDx26"
 
-# Memanggil fungsi untuk menambahkan musik latar
-add_background_music(music_url)
+# Menambahkan musik latar
+st.audio(music_url, format="audio/mp3", autoplay=True)
